0613845bdee1bd08ad5b7e836e139fa000d06f06
zqmillet <zqmillet@qq.com>
2020-09-27 23:44:53 +0800

    [bugfix] fix the bug of FileHandler in windows.

--- testcases/logger/conftest.py
+++ testcases/logger/conftest.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+import logging
 import pytest
 
 @pytest.fixture(scope='function', name='file_path')
@@ -11,6 +12,6 @@ def __file_path():
     assert not os.path.isfile(file_path)
     yield file_path
 
+    logging.shutdown() 
     if os.path.isfile(file_path):
         os.remove(file_path)
-
