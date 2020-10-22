0cc69176af144eb8cad13faaa200b307f5b91590
zqmillet <zqmillet@qq.com>
2020-10-23 00:14:52 +0800

    [test] add some testcases for the function load_yaml.

--- fast_tornado/functions/__init__.py
+++ fast_tornado/functions/__init__.py
@@ -3,3 +3,4 @@ description: this module provides some common functions for ``fast_tornado``.
 """
 
 from .wrap_text import wrap_text
+from .load_yaml import load_yaml
--- testcases/functions/load_yaml_test.py
+++ testcases/functions/load_yaml_test.py
@@ -1,1 +1,35 @@
+import os
+import pytest
+
 from fast_tornado.functions import load_yaml
+
+@pytest.mark.parametrize(
+    'content, data', [
+        [
+            'a',
+            'a'
+        ],
+        [
+            'a: 1',
+            {'a': 1}
+        ],
+        [
+            'a: "1"',
+            {'a': '1'}
+        ],
+        [
+            '[1, 2, 3]',
+            [1, 2, 3]
+        ],
+        [
+            '''
+            - 1
+            - 2
+            - 3
+            ''',
+            [1, 2, 3]
+        ]
+    ]
+)
+def test_load_yaml_from_string_without_exception(content, data):
+    assert load_yaml(content=content) == data
