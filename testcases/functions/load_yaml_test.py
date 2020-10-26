75c1f5aeaf48bd1791a543a8930887bd29091bcd
zqmillet <zqmillet@qq.com>
2020-10-26 23:46:37 +0800

    [test] add testcases for the function load_yaml.

--- testcases/functions/load_yaml_test.py
+++ testcases/functions/load_yaml_test.py
@@ -2,6 +2,7 @@ import os
 import pytest
 
 from fast_tornado.functions import load_yaml
+from fast_tornado.exceptions import InvalidYamlException
 
 @pytest.mark.parametrize(
     'content, data', [
@@ -33,3 +34,27 @@ from fast_tornado.functions import load_yaml
 )
 def test_load_yaml_from_string_without_exception(content, data):
     assert load_yaml(content=content) == data
+
+@pytest.mark.parametrize(
+    'content, exception_message', [
+        [
+            '''
+            a:
+            b
+            ''',
+            'while scanning a simple key'
+        ],
+        [
+            '''
+            a: 1
+              b: 2
+            ''',
+            'mapping values are not allowed here'
+        ]
+    ]
+)
+def test_load_yaml_from_string_with_exception(content, exception_message):
+    with pytest.raises(InvalidYamlException) as execution_infomation:
+        load_yaml(content=content)
+
+    assert exception_message in str(execution_infomation.value)
